8983325a71e39d18b4f831c3bf1b0203f1b760da
Allan Chau <allan.chau@icloud.com>
2018-02-07 16:07:40 +1030

    Actually install Yarn

--- scripts/node.sh
+++ scripts/node.sh
@@ -12,3 +12,4 @@ npm install -g npm
 # https://yarnpkg.com/en/docs/install#linux-tab
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn

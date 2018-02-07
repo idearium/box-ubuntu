8034dddd4e100e0113be2564b850d1c5c63fd2db
Allan Chau <allan.chau@icloud.com>
2018-02-07 16:15:23 +1030

    Fix docker installation

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,7 +1,18 @@
 #!/bin/sh -eux
 
-# Install the latest version of Docker.
-curl -fsSL https://get.docker.com/ | sh
+# Set up the repository.
+sudo apt-get -y install apt-transport-https ca-certificates curl
+
+# Add Docker’s official GPG key.
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Get Docker CE.
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce
 
 # Add docker to sudoers group.
 sudo usermod -aG docker vagrant
+
+# Start docker on boot.
+sudo systemctl enable docker

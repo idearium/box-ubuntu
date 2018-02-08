af9f37d70845b512466cdbd069e32d274d890a69
Allan Chau <allan.chau@icloud.com>
2018-02-08 11:16:54 +1030

    Remove installing the latest npm version

--- scripts/node.sh
+++ scripts/node.sh
@@ -5,9 +5,6 @@
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-# Install the latest version of npm.
-npm install -g npm
-
 # Install Yarn
 # https://yarnpkg.com/en/docs/install#linux-tab
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -

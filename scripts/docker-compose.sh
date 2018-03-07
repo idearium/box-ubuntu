b5b4d34fe0ac9eb42b58a243fa1f4d80dd61f059
Allan Chau <allan.chau@icloud.com>
2018-03-07 16:22:16 +1030

    Update docker-compose

--- scripts/docker-compose.sh
+++ scripts/docker-compose.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -eux
 
 # Install Compose.
-sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+# https://docs.docker.com/compose/install/#install-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # now alias so we don't have to type docker-compose all the time, just dc
